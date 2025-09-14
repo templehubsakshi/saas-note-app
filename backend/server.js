8e224d6dd9a89fef1202db77cfefb4dceb321b27
Sakshi Gupta <sakshi.2327cse1279@kiet.edu>
2025-09-15 04:40:05 +0530

    nn

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,32 @@
+// import dotenv from "dotenv";
+// import app from "./src/app.js";
+// import connectDB from "./src/config/db.js";
+
+// // Load .env variables
+// dotenv.config();
+
+// const PORT = process.env.PORT || 3000;
+
+// // Connect to MongoDB first
+// connectDB().then(() => {
+//   // Start server only after DB is connected
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
 import dotenv from "dotenv";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-// Load .env variables
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
-
-// Connect to MongoDB first
+// Connect DB first
 connectDB().then(() => {
-  // Start server only after DB is connected
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log("MongoDB connected");
 });
+
+// Vercel serverless handler
+export default function handler(req, res) {
+  app(req, res);
+}
+
